e1a54686e059440d15cf49c43f429415c1302cbd
Stephen N. Karagan <stevekaragan@gmail.com>
2021-04-07 21:20:28 -0400

    vercel static version

--- src/mainmenu.js
+++ src/mainmenu.js
@@ -10,6 +10,9 @@ export default class MainMenu extends Component {
     return(
      <div>
        <h2>Main Menu</h2>
+       <h3>Time to make your monthly budget!</h3>
+       <h4>If you are a new user start with Create Budget Below</h4>
+       <br/>
        <NavLink to={'/newbudget'} >Create New Budget</NavLink>
        <br/>
        <NavLink to={'/newBudget'} >Update Current Budget</NavLink>
